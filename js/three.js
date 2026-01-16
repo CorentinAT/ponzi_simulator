ab8825678efc25e1d56d7b108d11dd5f871996b4
CorentinAT <corentin.al2004@gmail.com>
2026-01-16 15:20:44 +0100

    errreur syntaxe

--- js/three.js
+++ js/three.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { nextTier } from "script.js";
+import { nextTier } from "./script.js";
 
 const colors = [
   {
